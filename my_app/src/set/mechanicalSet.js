7dd9a3f3061c346a3fd1081f0fd85c38a9914a51
Aakash <aakashkoirala17@gmail.com>
2023-12-25 18:34:02 +0545

    sets

---
+++ my_app/src/set/mechanicalSet.js
@@ -0,0 +1,366 @@
+export const quizData =[
+    {
+        "question": "A flat surface can be produced by a lathe machine, if the cutting tool moves _____",
+        "answer": "b) perpendicular to the axis of rotation of workpiece",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) parallel to the axis of rotation of workpiece",
+            "b) perpendicular to the axis of rotation of workpiece",
+            "c) at an angle of 450",
+            "d) none of the above"
+        ]
+    },
+    {
+        "question": "What is the function of cone pulley drive in lathe machines?",
+        "answer": "b) Change the spindle speed",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) Drive the lead screw",
+            "b) Change the spindle speed",
+            "c) Drive the tail-stock",
+            "d) All of the above"
+        ]
+    },
+    {
+        "question": "Which of the following is a mechanism for mechanized movements of the carriage along longitudinal axis?",
+        "answer": "c) Apron",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) Cross-slide",
+            "b) Compound rest",
+            "c) Apron",
+            "d) Saddle"
+        ]
+    },
+    {
+        "question": "The function of taper turning process is to ____",
+        "answer": "a) reduce the diameter of a workpiece along its length",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) reduce the diameter of a workpiece along its length",
+            "b) reduce the diameter by removing material about an axis offset from the axis of workpiece",
+            "c) remove the material from end surface of a workpiece",
+            "d) all of the above"
+        ]
+    },
+    {
+        "question": "The process of bevelling sharp ends of a workpiece is called as ________",
+        "answer": "d) chamfering",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) knurling",
+            "b) grooving",
+            "c) facing",
+            "d) chamfering"
+        ]
+    },
+    {
+        "question": "In an isolated system, ________ can be transferred between the system and its surrounding.",
+        "answer": "d) neither energy nor mass",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) only energy",
+            "b) only mass",
+            "c) both energy and mass",
+            "d) neither energy nor mass"
+        ]
+    },
+    {
+        "question": "Which of the following is an extensive property?",
+        "answer": "a) Volume",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) Volume",
+            "b) Pressure",
+            "c) Viscosity",
+            "d) All of the above"
+        ]
+    },
+    {
+        "question": "How is absolute pressure measured?",
+        "answer": "a) Gauge pressure + Atmospheric pressure",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) Gauge pressure + Atmospheric pressure",
+            "b) Gauge pressure – Atmospheric pressure",
+            "c) Gauge pressure / Atmospheric pressure",
+            "d) None of the above"
+        ]
+    },
+    {
+        "question": "The extensive properties of a system, _______",
+        "answer": "c) depend upon the mass of the system",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) are independent of the mass of the system",
+            "b) depend upon temperature of the system",
+            "c) depend upon the mass of the system",
+            "d) none of the above"
+        ]
+    },
+    {
+        "question": "An isobaric process, has constant _____",
+        "answer": "b) pressure",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) density",
+            "b) pressure",
+            "c) temperature",
+            "d) volume"
+        ]
+    },
+    {
+        "question": "Which of the following energy conversion devices convert heat into work?",
+        "answer": "b) I. C engines",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) Electrical generators",
+            "b) I. C engines",
+            "c) Condensers",
+            "d) All of the above"
+        ]
+    },
+    {
+        "question": "Lancashire boilers are ______",
+        "answer": "b) internally fired boilers",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) externally fired boilers",
+            "b) internally fired boilers",
+            "c) both a. and b.",
+            "d) none of the above"
+        ]
+    },
+    {
+        "question": "Smoke tube boilers have large water to steam ratio, hence _____",
+        "answer": "b) are slow in operations",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) have high evaporation rates",
+            "b) are slow in operations",
+            "c) temperature stresses inducing failure of feed water arrangement are maximum",
+            "d) all of the above"
+        ]
+    },
+    {
+        "question": "Which of the following statements are false for reaction turbines?",
+        "answer": "c) 1 and 4",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) 1, 2 and 3",
+            "b) 2 and 4",
+            "c) 1 and 4",
+            "d) 1, 2, 3 and 4"
+        ]
+    },
+    {
+        "question": "In four stroke cycle engine, cycle is completed in ______",
+        "answer": "b) two revolutions of the crankshaft",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) two strokes of the piston",
+            "b) two revolutions of the crankshaft",
+            "c) three strokes of the piston",
+            "d) four revolutions of the crankshaft"
+        ]
+    },
+    {
+        "question": "Which type of bearings are known as anti friction bearings?",
+        "answer": "b) Rolling contact bearings",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) Sliding contact bearings",
+            "b) Rolling contact bearings",
+            "c) Both a. and b.",
+            "d) None of the above"
+        ]
+    },
+    {
+        "question": "In roller bearings which type of rolling elements are used?",
+        "answer": "d) All of the above",
+        "section": "A",
+        "mark": 1,
+        "options": [
+            "a) Cylindrical rollers",
+            "b) Taper rollers",
+            "c) Spherical rollers",
+            "d) All of the above"
+        ],
+    },
+
+    
+        {
+            "question": "Why is the load carrying capacity low in ball bearings?",
+            "answer": "c) only 4",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) 1 and 3",
+                "b) only 1",
+                "c) only 4",
+                "d) 2 and 4"
+            ]
+        },
+        {
+            "question": "Cylindrical roller bearings have greater _____",
+            "answer": "c) radial load carrying capacity",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) axial load carrying capacity",
+                "b) thrust load carrying capacity",
+                "c) radial load carrying capacity",
+                "d) all of the above"
+            ]
+        },
+        {
+            "question": "Which of the following statements is/are true?",
+            "answer": "a) Jaw clutch is a positive clutch",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) Jaw clutch is a positive clutch",
+                "b) The power transmitting capacity of a friction clutch is inversely proportional to the coefficient of friction between contacting surfaces",
+                "c) Driving and driven plates in friction clutches are held together due to radial force applied by the helical springs",
+                "d) All of the above"
+            ]
+        },
+        {
+            "question": "Determine torque transmitted on the pinion shaft if torque transmitted on gear shaft is 20 Nm. Consider Gear ratio = 4",
+            "answer": "b) 5 Nm",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) 8 Nm",
+                "b) 5 Nm",
+                "c) 80 Nm",
+                "d) 16 Nm"
+            ]
+        },
+        {
+            "question": "Which of the following statements is/are true for gear drives?",
+            "answer": "b) They are used to transmit power between non-intersecting and parallel shafts",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) They can be used for long centre distances",
+                "b) They are used to transmit power between non-intersecting and parallel shafts",
+                "c) They cannot be used for high reduction ratios",
+                "d) All of the above"
+            ]
+        },
+        {
+            "question": "In which power transmitting drive polygon effect is observed?",
+            "answer": "b) Chain drive",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) Belt drive",
+                "b) Chain drive",
+                "c) Both a. and b.",
+                "d) None of the above"
+            ]
+        },
+        {
+            "question": "What is meant by gear ratio?",
+            "answer": "a) The ratio of pinion speed and gear speed",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) The ratio of pinion speed and gear speed",
+                "b) The ratio of number of teeth on pinion and number of teeth on gear",
+                "c) Both a. and b.",
+                "d) None of the above"
+            ]
+        },
+        {
+            "question": "Which gears are used to connect two intersecting shaft axes?",
+            "answer": "c) Bevel gears",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) Crossed helical gear",
+                "b) Worm and worm wheel",
+                "c) Bevel gears",
+                "d) All of the above"
+            ]
+        },
+        {
+            "question": "In stress-strain diagram, up to proportional limit ______",
+            "answer": "c) stress is directly proportional to strain",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) stress is inversely proportional to strain",
+                "b) force is directly proportional to displacement",
+                "c) stress is directly proportional to strain",
+                "d) strain is directly proportional to stress"
+            ]
+        },
+        {
+            "question": "Deformation per unit length is called as ________",
+            "answer": "a) strain",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) strain",
+                "b) stress",
+                "c) modulus of elasticity",
+                "d) none of the above"
+            ]
+        },
+        {
+            "question": "Which of the following materials do not have a well defined yield point?",
+            "answer": "d) All of the above",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) Heat treated steel",
+                "b) Concrete",
+                "c) Carbon fiber",
+                "d) All of the above"
+            ]
+        },
+        {
+            "question": "Factor of safety is the ratio of _________",
+            "answer": "d) yield strength and working stress",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) working stress and ultimate strength",
+                "b) yield strength and endurance strength",
+                "c) ultimate strength and yield strength",
+                "d) yield strength and working stress"
+            ]
+        },
+        {
+            "question": "The elongation of a bar is 0.5 mm, when a tensile stress of 200 N/mm2 acts on it. Determine original length of a bar if modulus of elasticity is 150 x 103.",
+            "answer": "a) 375.93 mm",
+            "section": "A",
+            "mark": 1,
+            "options": [
+                "a) 375.93 mm",
+                "b) 300 mm",
+                "c) 360 mm",
+                "d) None of the above"
+            ]
+        }
+    
+    
+]
